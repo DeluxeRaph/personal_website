7706ce6bb26863f6c810016c79263fef0912aa49
Raphael Nembhard <nembhardraphael199@gmail.com>
2024-08-05 19:05:35 -0400

    added some text and changed background color

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,9 @@ struct MyObject {
 #[function_component(App)]
 pub fn app() -> Html {
     let name = "raph";
+    let title = "my_title";
+    let about= "my_about";
+    let img = "my_img";
     let my_object = MyObject {username: name.to_owned(), favorite_language: "Rust".to_owned()};
 
     log!(serde_json::to_string_pretty(&my_object).unwrap());
@@ -19,8 +22,27 @@ pub fn app() -> Html {
        <>
        <head></head>
         <body>
-            <h1><u>{"Raphael Nembhard Personal Website"}</u></h1>
+            <h1 class={title}><u>{"Raphael Nembhard Personal Website"}</u></h1>
+            <p>
+                <a href="https://opensea.io/assets/ethereum/0xd3605059c3ce9facf625fa72d727508b7b7f280f/1785">
+                <img class={img}src="img/villagefarmer.JPG" alt="rust image"/>
+                </a>
+                    <font>{"My PFP is my beloved Pablo #1785"}</font>
+            </p>
+            <div class={about}>{"I build then I build more"}
+                <br />
+                {"I like to build blockchains and dapps. My favorite langauges are Rust and soldity"}
+                <br />
+                {"You can view my projects here on my "}
+                <a href="https://github.com/DeluxeRaph">
+                {"Github"}</a>
+            </div>
         </body>
-       </>
+        <footer>
+            <hr />
+            <p>{"Author: Raphael"}</p>
+            <p><a href="mailto:raphael@digitalinnovation.com">{"raphael@digitalinnovation.com"}</a></p>
+        </footer>
+       </> 
     }
 }
